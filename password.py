4764e3e6be3dbf0ff1af7103536004b4e6b70573
gokulpb <gokulpbharathan@gmail.com>
2021-03-04 14:22:27 +0530

    Added password encryption

--- password.py
+++ password.py
@@ -1,6 +1,13 @@
+from cryptography.fernet import Fernet
+
 def get_password_input():
+    print("Generating key")
+    key = Fernet.generate_key()
+    f = Fernet(key)
     domain_name = input("What is the name of the site or domain: ")
     password = input(f"What is the password for {domain_name}: ")
-    print("Password added successfully") 
+    encrypted_password = f.encrypt(str.encode(password))
+    print("Password added successfully")   
+
 if __name__ == "__main__":
     get_password_input()
