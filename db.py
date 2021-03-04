b1de90eca54e391a488798f4274b26938259adad
gokulpb <gokulpbharathan@gmail.com>
2021-03-04 14:57:51 +0530

    Added menu and cursor

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import mariadb
 
-def connect_db():
+def connect_db(master_password=""):
     try:
         conn = mariadb.connect(
         user="root",
@@ -10,8 +10,12 @@ def connect_db():
         port=3306
         )
         print("Connected to db successfully")
+        cur = conn.cursor()
+        table = cur.execute("SELECT * FROM password")
+        print(table)
     except Exception as e:
         print(e)
+
 
 if __name__ == "__main__":
     connect_db()
---
+++ menu.py
@@ -0,0 +1,11 @@
+from db import connect_db 
+
+print(conn)
+
+
+menu = """
+    /////// WELCOME TO YOUR PASSWORD MANAGER /////
+            
+        """
+
+print(menu)
