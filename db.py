132d815d26ac0f4b5bab5ee784cb5084b445b12b
gokulpb <gokulpbharathan89@gmail.com>
2021-03-08 20:48:21 +0530

    Added new password functionality and restricting people after 1 wrong attempt

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 import mariadb
 from menu import get_master_password
 from secret import random_text_mp
+import os 
 
 conn = mariadb.connect(
 user="root",
@@ -12,6 +13,15 @@ port=3306
 print("Connected to db successfully")
 cur = conn.cursor()
  
+def add_new_password():
+    domain = input("What is the name of the site or domain: ")
+    password = input(f"What is the password for {domain}: ")
+    sql = f"""INSERT INTO password (domain, password) VALUES ('{domain}', '{password}')"""
+    cur.execute(sql)
+    conn.commit()
+    print("Success")
+    conn.close()
+
 def add_masterpassword():       
     masterkey_table = cur.execute("SELECT * FROM MasterPassword;")
     masterpassword = cur.fetchall()
@@ -25,14 +35,18 @@ def add_masterpassword():
         print("successfully added")
         conn.close()
 
+
     else:
         user_masterpassword = input("What is the master password: ")
         hashed_user_masterpassword = str.join(user_masterpassword, random_text_mp)
         for master_tuple in masterpassword:
             for m in master_tuple:
                 if m == hashed_user_masterpassword:
-                    print("Successfully logged in")
-                 
+                    print("Successfully logged in")               
+                    add_new_password()
+                else:
+                    print("Later man go find your password")
+                    os._exit(0)                    
 
 add_masterpassword()
 
