7499d3a910fb941b2e5daf42d9b71b4cb9f9e1bd
gokulpb <gokulpbharathan89@gmail.com>
2021-03-08 16:06:25 +0530

    Added randomkey queries

--- db.py
+++ db.py
@@ -11,22 +11,21 @@ port=3306
 print("Connected to db successfully")
 cur = conn.cursor()
  
-def add_master_password():       
+def add_random_key():       
     randomkey_table = cur.execute("SELECT * FROM randomkey;")
-    print("Hello")
     randomkey = cur.fetchall()
-    print(randomkey)
-    for r in randomkey:
-        if r is None:  
-            random_words = input("Type some random words: ") 
-            sql = f"""INSERT INTO randomkey (randomkey) VALUES ("{random_words}")"""
-            cur.execute(sql)
-            conn.commit()
-            conn.close()
-            print("Successfully added random words")
+    if len(randomkey) == 0:
+        random_words = input("Type some random words: ") 
+        sql = f"""INSERT INTO randomkey (randomkey) VALUES ("{random_words}")"""
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+        print("Successfully added random words")
 
-        elif randomkey is not None:
-            print("Random key is present")
+    else:
+        for r in randomkey:
+            print(r)
+        
 
 add_master_password()
 
